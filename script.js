ae1ff50b053c36cf56ff70dc291cc4102f1bd906
Cesario87 <ces.albred@gmail.com>
2023-01-03 13:01:26 +0100

    1st phase completed

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ async function showLists() {
         let tarjeta = document.createElement("div")
         tarjetas.appendChild(tarjeta);
 
-        tarjeta.innerHTML = `<div id="tarjetas">
+        tarjeta.innerHTML = `<div id="tarjetasListas">
         <p>${listasTodas[i].list_name}</p>
         <p>Oldest: ${listasTodas[i].oldest_published_date}</p>
         <p>Newest: ${listasTodas[i].newest_published_date}</p>
@@ -38,19 +38,51 @@ async function showLists() {
         let nombreLista = listasTodas[i].list_name;
         document.querySelector(`#btn${[i]}`).addEventListener("click", () => { init(nombreLista, tarjetas); })
     }
-    async function getList(nombre) {
+    async function getOneList(nombre) {
         let resultado = await fetch(`https://api.nytimes.com/svc/books/v3/lists/current/${nombre}.json?api-key=RWybTfef3RewOzxt4nu6khx24FcPaoAI`);
         let dataBase = await resultado.json();
         const lista1 = dataBase.results;
         return lista1;
     }
-    
+
     async function init(nombre, espacio) {
-        let lista1 = await getList(nombre);
+        let lista1 = await getOneList(nombre);
         console.log(lista1);
         console.log(nombre);
-        espacio.innerHTML = "perrito OOOHH YESSS"
+        espacio.innerHTML = "";
+        let botonIndex = document.createElement("div");
+        espacio.appendChild(botonIndex);
+        botonIndex.innerHTML = `<input type="button" value="BACK TO INDEX" id="btnIndex"></input>`;
+        
+        const botonIndex1 = document.querySelector("#btnIndex");
+        botonIndex1.onclick = () => {
+            espacio.innerHTML = "";
+            showLists()
+        };
+
+        for (let j = 0; j < lista1.books.length; j++) {
+            let espacioLibros = document.createElement("div");
+            espacio.appendChild(espacioLibros);
+            espacioLibros.innerHTML = `<div id="tarjetasLibros">
+            <p>#${lista1.books[j].rank} ${lista1.books[j].title}</p>
+            <img id="portada" src="${lista1.books[j].book_image}"></img>
+            <p>Weeks on list: ${lista1.books[j].weeks_on_list}</p>
+            <p>${lista1.books[j].description}</p>
+            <input type="button" value="FAV" id="btnFav"></input>
+            <a href="${lista1.books[j].amazon_product_url}"><img id="iconoAmazon" src="amazon.jpg"></img></a>
+            </div>`;
+        }
+
+        const botonFav = document.querySelector("#btnFav");
+        botonFav.onclick = () => {
+            //GUARDAR FAVORITO EN FIREBASE
+        };
     }
 }
 
 showLists();
+
+const loaderContainer = document.querySelector('.loader-container');
+window.addEventListener('load', () => {
+    loaderContainer.style.display = 'none';
+});
